5f1b1fa49237384e2c0e168768085e287318af86
RichardBray <richardbray89@gmail.com>
2023-11-01 15:45:51 +0000

    chore: code for 4-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { lazy } from 'react';
 import './App.css';
 import BlogPost from './components/BlogPost';
 import Card from './components/Card';
-import Details from './components/Details';
+// import Details from './components/Details';
 import { videos } from './video-data';
 
+const Details = lazy(() => import('./components/Details'));
+
 const App = () => {
-  return (
+  const showDetails = true;
+
+  return showDetails ? (
+    <Details />
+  ) : (
     <>
       {videos.map((video) => (
         <Card key={video.id} video={video} />
