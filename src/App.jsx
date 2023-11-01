b289d403d0d2aef6f0e4507ccb82847e1d86827b
RichardBray <richardbray89@gmail.com>
2023-11-01 11:52:22 +0000

    chore: add quiz solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css';
+import BlogPost from './components/BlogPost';
 import Card from './components/Card';
 import Details from './components/Details';
 
 const App = () => {
-  return <Details />;
+  return (
+    <BlogPost title="5 Simple Steps to Maximise Your Home Garden's Harvest">
+      <p>
+        {' '}
+        Transform your home garden into a thriving haven for fresh produce! In this blog post, we'll reveal five
+        easy-to-follow steps to help you achieve a bountiful harvest.
+      </p>
+      <p>
+        Discover how to select the right crops, optimize your planting space, and implement sustainable practices for
+        long-lasting success. Say goodbye to wilted leaves and hello to an abundant, homegrown harvest that will impress
+        your friends and family!
+      </p>
+    </BlogPost>
+  );
 };
 
 export default App;
---
+++ src/components/BlogPost.jsx
@@ -0,0 +1,10 @@
+const BlogPost = ({ title, children }) => {
+  return (
+    <>
+      <h1>{title}</h1>
+      <div>{children}</div>
+    </>
+  );
+};
+
+export default BlogPost;
