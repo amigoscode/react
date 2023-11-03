41d8a583224e5e0c990741043316ea41e1569fbd
RichardBray <richardbray89@gmail.com>
2023-11-03 17:10:43 +0000

    chore: code for 5-4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Suspense, lazy } from 'react';
 import Card from './components/Card';
 import { posts } from './tech-data';
-import './App.css';
+import styles from './App.module.css';
 
 const firstThree = posts.slice(0, 3);
 const lastTwo = posts.slice(3, 5);
@@ -10,10 +10,10 @@ const LastTwo = lazy(() => import('./components/LastTwo'));
 
 const App = () => {
   const darkMode = false;
-  const styles = darkMode ? 'dark-mode' : 'light-mode';
+  const darkModeClass = darkMode ? styles['dark-mode'] : styles['light-mode'];
 
   return (
-    <div className={`wrapper ${styles}`}>
+    <div className={`${styles.wrapper} ${darkModeClass}`}>
       {firstThree.map((post) => (
         <Card key={post.id} video={post} style={{ color: 'red' }} />
       ))}
