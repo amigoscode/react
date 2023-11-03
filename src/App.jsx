3dfd2e74333a8679cbb3d138660ca75656103c8b
RichardBray <richardbray89@gmail.com>
2023-11-03 15:54:56 +0000

    chore: add styles for 5-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Suspense, lazy } from 'react';
-import './App.css';
 import Card from './components/Card';
 import { posts } from './tech-data';
 
@@ -9,15 +8,18 @@ const lastTwo = posts.slice(3, 5);
 const LastTwo = lazy(() => import('./components/LastTwo'));
 
 const App = () => {
+  const darkMode = true;
+  const styles = darkMode ? { color: 'white', backgroundColor: 'black' } : { color: 'black', backgroundColor: 'white' };
+
   return (
-    <>
+    <div style={{ ...styles, fontFamily: 'sans-serif', textAlign: 'center' }}>
       {firstThree.map((post) => (
-        <Card key={post.id} video={post} />
+        <Card key={post.id} video={post} style={{ color: 'red' }} />
       ))}
       <Suspense fallback={<div>Loading...</div>}>
         <LastTwo data={lastTwo} />
       </Suspense>
-    </>
+    </div>
   );
 };
 
