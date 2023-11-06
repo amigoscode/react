c3d09dacadb6c8e19064c567f079314a951e25de
RichardBray <richardbray89@gmail.com>
2023-11-06 15:53:15 +0000

    chore: finish code for 6-3

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Card from './components/Card';
 import { posts } from './tech-data';
 import styles from './App.module.css';
 import Counter from './components/Counter';
+import CounterTwo from './components/CounterTwo';
 
 const App = () => {
   return (
     <div className={styles.wrapper}>
-      <Counter />
+      <CounterTwo />
     </div>
   );
 };
