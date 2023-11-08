5d812ad9b99a85f7123da9375ba8d9d94d42c4ab
RichardBray <richardbray89@gmail.com>
2023-11-08 17:09:30 +0000

    chore: finish 7-3

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    console.log('component mounted');
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (mounted) {
+      console.log('counter value change');
+    }
+    return () => console.log('component has unmounted');
+  }, [count]);
+
   return (
     <section>
       <h1>{count}</h1>
+      <h1>Count + 1:{count + 1}</h1>
+      <h1>Count + 2:{count + 2}</h1>
       <button onClick={() => setCount((prevCount) => prevCount + 1)}>+</button>
       <button onClick={() => count > 0 && setCount((prevCount) => prevCount - 1)}>-</button>
     </section>
