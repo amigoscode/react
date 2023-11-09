24fcf7fdbeef465d69fc24ec3bb433e33f104c28
RichardBray <richardbray89@gmail.com>
2023-11-09 17:20:55 +0000

    chore: change pokemon words to star wars

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const App = () => {
-  const [pokemonList, setPokemonList] = useState(null);
+  const [starWarsList, setStarWarsList] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { data } = await axios.get('https://swapi.dev/api/people');
-        setPokemonList(data);
+        const response = await fetch('https://swapi.dev/api/people');
+        const data = await response.json();
+        setStarWarsList(data);
       } catch (error) {
         console.error(error);
       }
     };
     fetchData();
   }, []);
-  if (!pokemonList?.results) {
+  if (!starWarsList?.results) {
     return <div>Loading...</div>;
   } else {
     return (
       <>
-        <h1>Pokemon</h1>
-        {pokemonList.results.map((pokemon) => (
-          <article key={pokemon.name}>
-            <h2>{pokemon.name}</h2>
-            <p>gender: {pokemon.gender}</p>
-            <p>birth year: {pokemon.birth_year}</p>
-            <p>eye color: {pokemon.eye_color}</p>
-            birth_year
+        <h1>Star Wars List</h1>
+        {starWarsList.results.map((starWars) => (
+          <article key={starWars.name}>
+            <h2>{starWars.name}</h2>
+            <p>gender: {starWars.gender}</p>
+            <p>birth year: {starWars.birth_year}</p>
+            <p>eye color: {starWars.eye_color}</p>
           </article>
         ))}
       </>
