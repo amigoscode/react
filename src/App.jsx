1a3a51100665aaa8300cdc5e0833c6e7412e0c0f
RichardBray <richardbray89@gmail.com>
2023-11-09 16:53:38 +0000

    chore: put back end code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 const App = () => {
-  return <h1>Star Wars People</h1>;
+  const [pokemonList, setPokemonList] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get('https://swapi.dev/api/people');
+        setPokemonList(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+  if (!pokemonList?.results) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <>
+        <h1>Pokemon</h1>
+        {pokemonList.results.map((pokemon) => (
+          <article key={pokemon.name}>
+            <h2>{pokemon.name}</h2>
+            <p>gender: {pokemon.gender}</p>
+            <p>birth year: {pokemon.birth_year}</p>
+            <p>eye color: {pokemon.eye_color}</p>
+            birth_year
+          </article>
+        ))}
+      </>
+    );
+  }
 };
 
 export default App;
