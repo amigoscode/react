bda09524b6662f5e037f412d09d761d473420aad
RichardBray <richardbray89@gmail.com>
2023-11-09 16:54:22 +0000

    chore: add starting code

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,5 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
-
 const App = () => {
-  const [pokemonList, setPokemonList] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data } = await axios.get('http://pokeapi.co/api/v2/pokemon');
-        setPokemonList(data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  }, []);
-  if (!pokemonList?.results) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <>
-        <h1>Pokemon</h1>
-        {pokemonList.results.map((pokemon) => (
-          <div key={pokemon.name}>{pokemon.name}</div>
-        ))}
-      </>
-    );
-  }
+  return <h1>Star Wars People</h1>;
 };
 
 export default App;
