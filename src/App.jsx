979d844cb32e32356025fa043546558b1d631541
RichardBray <richardbray89@gmail.com>
2023-11-09 14:31:50 +0000

    chore: add code for 7-8

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
-import { Suspense, lazy, useReducer, useState } from 'react';
 import Card from './components/Card';
-import { posts } from './tech-data';
+import { videos } from './video-data.js';
 import styles from './App.module.css';
-import Counter from './components/Counter';
-import CounterTwo from './components/CounterTwo';
-import { CounterContext } from './CounterContext';
-
-const initialState = {
-  countOne: 0,
-  countTwo: 0,
-};
-
-const counterReducer = (state, action) => {
-  const actions = {
-    increment: () => ({ ...state, countOne: state.countOne + 1 }),
-    decrement: () => ({ ...state, countTwo: state.countTwo - 1 }),
-    reset: () => initialState,
-  };
-
-  return actions[action] ? actions[action]() : state;
-};
 
 const App = () => {
-  const value = useReducer(counterReducer, initialState);
   return (
-    <CounterContext.Provider value={value}>
-      <Counter />
-    </CounterContext.Provider>
+    <div className={styles.wrapper}>
+      {videos.map((video) => (
+        <Card key={video.id} video={video} />
+      ))}
+    </div>
   );
 };
 
