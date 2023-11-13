4bf010978cdb02d18d8c575abb7352662d23ee87
RichardBray <richardbray89@gmail.com>
2023-11-13 15:34:04 +0000

    chore: finish code for 8-3

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import AboutUs from './pages/AboutUs.jsx';
+import styles from './App.module.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/about-us',
+    element: <AboutUs />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <div className={styles.wrapper}>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
