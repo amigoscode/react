5a70a3c73b1e188f39a3d9e3aa575d1424572568
RichardBray <richardbray89@gmail.com>
2023-11-15 15:08:54 +0000

    chore: finish 10-2

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,7 +1,22 @@
+import styles from './ContactUs.module.css';
+
 const ContactUs = () => {
   return (
     <>
       <h1>Contact Us</h1>
+      <form>
+        <section>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input className={styles.email} type="email" id="email" name="email" placeholder="test@test.com" />
+        </section>
+        <section>
+          <label htmlFor="message">Message</label>
+          <br />
+          <textarea name="message" id="message" className={styles.message} placeholder="What's on your mind" />
+        </section>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
