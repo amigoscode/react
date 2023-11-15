65b2d7a4f0310aa7a5f11f9a4b8e5f1970335df8
RichardBray <richardbray89@gmail.com>
2023-11-15 17:13:03 +0000

    chore: 10-5 end

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,47 @@
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
+
 const HomePage = () => {
+  const [todoItem, setTodoItem] = useState('');
+  const [todoItems, setTodoItems] = useState([]);
+
+  function handleSubmit(eventObj) {
+    eventObj.preventDefault();
+    setTodoItems([...todoItems, { todo: todoItem, id: nanoid() }]);
+    setTodoItem('');
+  }
+
   return (
     <>
       <h1>Todo List</h1>
-      <form>
-        <section style={{ marginTop: '1rem' }}>
-          <label htmlFor="todoTask">Task</label>
+      <form onSubmit={handleSubmit}>
+        <section>
+          <label htmlFor="todoTask">
+            <strong>New task</strong>
+          </label>
           <br />
-          <input style={{ width: '13rem' }} type="text" id="todoTask" name="todo" placeholder="buy chocolate" />
+          <input
+            id="todoTask"
+            name="todo"
+            placeholder="buy chocolate"
+            onChange={(e) => setTodoItem(e.target.value)}
+            value={todoItem}
+          />
         </section>
       </form>
-      <section style={{ marginTop: '1rem', width: '14rem' }}>
-        <strong>Tasks</strong>
+      <section>
+        {todoItems.length === 0 ? (
+          <div>Add a todo 👆</div>
+        ) : (
+          <>
+            <strong>Tasks</strong>
+            <ul>
+              {todoItems.map((todo) => (
+                <li key={todo.id}>{todo.todo} </li>
+              ))}
+            </ul>
+          </>
+        )}
       </section>
     </>
   );
