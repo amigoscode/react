fb4aabd8d778179abe43fed1ec1fefe56d8d6e0f
RichardBray <richardbray89@gmail.com>
2023-11-16 11:46:22 +0000

    chore: finish code for 11-5

--- src/pages/HomePage.test.jsx
+++ src/pages/HomePage.test.jsx
@@ -1,7 +1,16 @@
 import HomePage from './HomePage';
 import { render, screen } from '@testing-library/react';
 
-test('renders HomePage', () => {
-  render(<HomePage />);
-  screen.debug();
+describe('HomePage', () => {
+  test('renders the title', () => {
+    render(<HomePage />);
+    const title = screen.getByRole('heading', { name: 'Todo List' });
+    expect(title).toBeInTheDocument();
+  });
+
+  test('renders the input', () => {
+    render(<HomePage />);
+    const input = screen.getByRole('textbox', { name: 'New task' });
+    expect(input).toBeInTheDocument();
+  });
 });
