bfaf4810cf61aee61bd89e68ed8e11256de3c79b
RichardBray <richardbray89@gmail.com>
2023-11-16 11:48:48 +0000

    chore: setup for tests

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5000
+  },
+  test: {
+    global: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js'
   }
 })
